5bdb92fc34472351277cabdae7efdb00d37efdfe
KenetGaona115 <aldrinkenet@hotmail.com>
2023-04-22 03:01:29 -0600

    add ussles code

--- backend/database/db.database.js
+++ backend/database/db.database.js
@@ -1,35 +1,43 @@
 const helper = require('./helper');
-
 const db = require('../database/db.user.json');
 
-
 async function buscar(idcard) {
-    let person =await getExistidCardPerson(idcard);
+    let person = await getExistidCardPerson(idcard);
     //
-    
+
     return person;
-    
+
 }
 function getAllUsers() {
     return db.users;
 }
 
-function getUser(idU){
+function getUser(idU) {
     const data = helper.getJSON();
     console.log(data)
-    const foundUser  = data.users.find((user) => user.id == idU)
+    const foundUser = data.users.find((user) => user.id == idU)
     let user
-    if(foundUser){
+    if (foundUser) {
         user = foundUser
     }
-    else{
-        user = {mensaje:"usuario no encontrado"}
+    else {
+        user = { mensaje: "usuario no encontrado" }
     }
     return user;
+}
 
+function getSesion(email, password) {
+    const data = helper.getJSON();
+    const foundUser = data.users.find((user) => user.correo == email && user.password == password)
+
+    if (foundUser)
+        return { code: 200, message: 'El usuario si existe' }
+    else
+        return { code: 400, message: 'error, usuario o contraseña incorrectos' }
 }
 
 module.exports = {
-    getAllUsers:getAllUsers,
-    getUser:getUser
+    getAllUsers: getAllUsers,
+    getUser: getUser,
+    getSesion: getSesion
 }
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,21 +2,23 @@ const express = require('express')
 const router = express.Router()
 const db = require('../database/db.database')
 
-router.get("/getallusers", async (req, res, next)=>{
-    const usuarios = await db.getAllUsers()
-    res.send(usuarios)
- })
+router.get("/getallusers", async (req, res, next) => {
+   const usuarios = await db.getAllUsers()
+   res.send(usuarios)
+})
 
- router.get("/getuserbyid", async(req, res, next)=>{
-    const { id } = req.query
-    const usuario = await db.getUser(id)
-    
-    res.send(usuario)
- })
+router.get("/getuserbyid", async (req, res, next) => {
+   const { id } = req.query
+   const usuario = await db.getUser(id)
 
- router.get("/Login", async(req, res, next)=>{
+   res.send(usuario)
+})
 
- })
+router.get("/Login", async (req, res, next) => {
+   const { correo, password } = req.query
+   console.log(correo, password)
+   res.send(await db.getSesion(correo, password))
+})
 
 
 module.exports = router;
